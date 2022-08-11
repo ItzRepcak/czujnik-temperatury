531cb6d37e118e2ec3a0bf62d9dbffee0cc1541a
ItzRepcak <111084780+ItzRepcak@users.noreply.github.com>
2022-08-11 21:18:09 +0200

    Aktualizacja main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("" + (input.temperature()))
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (input.lightLevel()))
+})
 basic.forever(function () {
 	
 })
